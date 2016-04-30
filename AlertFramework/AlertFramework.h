20dad01035a1397c3fd7eb98fba8ba3b26efcf31
Alcala, Jose Luis <JoseLuis.Alcala@adidas-group.com>
2016-04-30 21:24:47 +0200

    Shared Framework

---
+++ AlertFramework/AlertFramework.h
@@ -0,0 +1,19 @@
+//
+//  AlertFramework.h
+//  AlertFramework
+//
+//  Created by Alcala, Jose Luis on 4/30/16.
+//  Copyright © 2016 Mark Miscavage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlertFramework.
+FOUNDATION_EXPORT double AlertFrameworkVersionNumber;
+
+//! Project version string for AlertFramework.
+FOUNDATION_EXPORT const unsigned char AlertFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertFramework/PublicHeader.h>
+
+
